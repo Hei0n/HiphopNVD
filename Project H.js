76db9f83645c8870ef6cf8952051d42188116751
Binh <bluongthanh@gmail.com>
2019-09-03 20:14:55 +0700

    Hiphop

---
+++ Project H.js
@@ -0,0 +1,24 @@
+// Scroll to element
+
+//Scroll to button
+function rapScroll() {
+    let rap = document.getElementById('firstPara')
+    rap.scrollIntoView();
+}
+
+function graffitiScroll() {
+    let graffiti = document.getElementById('secondPara')
+    graffiti.scrollIntoView();
+}
+
+function djScroll() {
+    let dj = document.getElementById('thirdPara')
+    dj.scrollIntoView();
+}
+
+function hiphopdanceScroll() {
+    let hiphopdance = document.getElementById('fourthPara')
+    hiphopdance.scrollIntoView();
+}
+
+
